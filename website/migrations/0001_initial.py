aa45c8dc1ee3a1a8588e61d774ef341d898272c9
Kiiwi <tommy.lee.ryan@gmail.com>
2015-08-15 18:51:34 +0200

    changed database settings

--- website/migrations/0001_initial.py
+++ website/migrations/0001_initial.py
@@ -13,10 +13,10 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='Registration',
             fields=[
-                ('id', models.AutoField(primary_key=True, verbose_name='ID', serialize=False, auto_created=True)),
+                ('id', models.AutoField(auto_created=True, serialize=False, verbose_name='ID', primary_key=True)),
                 ('team_name', models.CharField(max_length=100)),
                 ('team_from', models.CharField(max_length=100)),
-                ('tournament_class', models.CharField(max_length=100, default='Herrer', choices=[('H', 'Herrer'), ('D', 'Damer'), ('A', 'Åpen')])),
+                ('tournament_class', models.CharField(default='Herrer', max_length=100, choices=[('H', 'Herrer'), ('D', 'Damer'), ('A', 'Åpen')])),
                 ('email', models.CharField(max_length=100)),
                 ('phone', models.CharField(max_length=15)),
                 ('other', models.TextField(max_length=256, blank=True)),
@@ -26,17 +26,17 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='Team',
             fields=[
-                ('id', models.AutoField(primary_key=True, verbose_name='ID', serialize=False, auto_created=True)),
+                ('id', models.AutoField(auto_created=True, serialize=False, verbose_name='ID', primary_key=True)),
                 ('team_name', models.CharField(max_length=100)),
                 ('team_from', models.CharField(max_length=100)),
-                ('tournament_class', models.CharField(max_length=100, default='Herrer', choices=[('H', 'Herrer'), ('D', 'Damer'), ('A', 'Åpen')])),
+                ('tournament_class', models.CharField(default='Herrer', max_length=100, choices=[('H', 'Herrer'), ('D', 'Damer'), ('A', 'Åpen')])),
                 ('timestamp', models.DateField(auto_now_add=True)),
             ],
         ),
         migrations.CreateModel(
             name='TournamentStatus',
             fields=[
-                ('id', models.AutoField(primary_key=True, verbose_name='ID', serialize=False, auto_created=True)),
+                ('id', models.AutoField(auto_created=True, serialize=False, verbose_name='ID', primary_key=True)),
                 ('signup', models.BooleanField(default=True)),
                 ('pre_start', models.BooleanField()),
                 ('started', models.BooleanField()),
